b02473f6181022accf98cabec012495510fb45af
PokerDoDigital <pokerdodigital@gmail.com>
2025-02-25 12:12:02 -0300

    Terceiro commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Habilitar o parsing de JSON
+app.use(express.json());
+
+// Rota principal
 app.get("/", (req, res) => {
+  res.send("<h1>Servidor rodando com sucesso!</h1>");
+});
+
+// Rota API
+app.get("/api", (req, res) => {
   res.json({ message: "Servidor rodando com sucesso!" });
 });
 
